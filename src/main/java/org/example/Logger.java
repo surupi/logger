1b4caeeebae2791256abfaf38052b9d82aa38f37
surupi <surupin@rssoftware.co.in>
2024-10-07 09:28:28 +0530

    WIP

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -4,8 +4,7 @@ import java.util.function.Function;
 @FunctionalInterface
 public interface Logger {
     void log(String message);
-
-    // Default Methods
+    
     default void logWithPrefix(String prefix, String message) {
         log(prefix + ": " + message);
     }
@@ -14,50 +13,8 @@ public interface Logger {
         log(message.toUpperCase());
     }
 
-    default void logWithTransformation(Function<String, String> transformer, String message) {
-        log(transformer.apply(message));
-    }
-
-    default void logInLeetSpeak(String message) {
-        String leetMessage = message
-                .replace("e", "3")
-                .replace("a", "4")
-                .replace("t", "7")
-                .replace("o", "0")
-                .replace("s", "5");
-        log(leetMessage);
-    }
-
-    default void logWithCaesarCipher(String message, int shift) {
-        StringBuilder ciphered = new StringBuilder();
-        for (char c : message.toCharArray()) {
-            if (Character.isLetter(c)) {
-                char shifted = (char) (c + shift);
-                ciphered.append(shifted);
-            } else {
-                ciphered.append(c);
-            }
-        }
-        log(ciphered.toString());
-    }
-
-    default void logInReverse(String message) {
-        String reversed = new StringBuilder(message).reverse().toString();
-        log(reversed);
-    }
-
     default void logInColor(String message, String colorCode) {
-        log(colorCode + message + "\u001B[0m"); // Reset color
-    }
-
-    default void logWithStyle(String message, String colorCode, String backgroundColor, String effect) {
-        log(colorCode + backgroundColor + effect + message + "\u001B[0m"); // Reset color and effect
-    }
-
-    default void logWithRepeatedMessage(String message, int repeatCount) {
-        for (int i = 0; i < repeatCount; i++) {
-            log(message);
-        }
+        log(colorCode + message + "\u001B[0m");
     }
 
     default void logWithoutVowels(String message) {
--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -8,25 +8,9 @@ public class LoggerTest {
     public static void main(String[] args) {
         Logger logger = Logger.getDefaultLogger();
 
-        logger.log("This is a red message!");
-        logger.logInUppercase("this is an uppercase message.");
-        logger.logInLeetSpeak("leet speak message");
-        logger.logWithCaesarCipher("Hello World", 3);
-        logger.logInReverse("Reverse this message");
-        logger.logInColor("This is green text", ColorCodes.GREEN);
-        logger.logWithStyle("This is a bold green message on a blue background", ColorCodes.GREEN, ColorCodes.BLUE_BG, ColorCodes.BOLD);
-        logger.logWithRepeatedMessage("Repeated message", 3);
-        logger.logWithoutVowels("Hello, how are you?");
-
-
-        Function<String, String> transformation = message -> {
-            return message.replace("Hello", "Hi");
-        };
-        Logger transformationLogger = (message) -> {
-            String transformedMessage = transformation.apply(message);
-            System.out.println(transformedMessage);
-        };
-        transformationLogger.log("Hello world!");
+//        logger.log("This is a red message!");
+//        logger.logInUppercase("this is an uppercase message.");
+//        logger.logInColor("This is green text", ColorCodes.GREEN);
 
     }
 }
